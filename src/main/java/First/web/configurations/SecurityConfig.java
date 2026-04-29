f92c3cd158b28fe2605b7d96d3030a080bdd8f5e
Михаил Терёхин <b8140393@gmail.com>
2026-04-29 10:19:42 +0300

    добавил фон

--- src/main/java/First/web/configurations/SecurityConfig.java
+++ src/main/java/First/web/configurations/SecurityConfig.java
@@ -28,8 +28,8 @@ public class SecurityConfig {
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/registration","/images/**").permitAll()
-                        .requestMatchers("/product/**", "/image/**","/user/**","/static/**","/my/products")
+                        .requestMatchers("/", "/registration","/images/**","/static/**").permitAll()
+                        .requestMatchers("/product/**", "/image/**","/user/**","/my/products")
                         .hasAnyAuthority("ROLE_ADMIN","ROLE_USER")
                         .anyRequest().authenticated()
                 )
